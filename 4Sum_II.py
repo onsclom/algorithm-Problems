055797e8f4b00d34a2c0e73e9caf46e84306ddbc
Austin Merrick <austinmerrick98@gmail.com>
2019-08-31 12:19:23 -0700

    4sum_ii

---
+++ 4Sum_II.py
@@ -0,0 +1,23 @@
+# 4sum_ii  on leetcode
+# https://leetcode.com/problems/4sum-ii/
+
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        
+        myDict = {}
+        amount = 0
+        
+        for i in A:
+            for j in B:
+                if (i+j) in myDict:
+                    myDict[i+j] += 1
+                else:
+                    myDict[i+j] = 1
+                    
+        for i in C:
+            for j in D:
+                if -1*(i+j) in myDict:
+                    amount += myDict[-1*(i+j)]
+                    
+        return amount
+        
