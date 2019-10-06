9a2ab01913a083dc22cb5862f5770c12029c16f0
Austin Merrick <austinmerrick98@gmail.com>
2019-10-05 20:26:58 -0700

    Fixing indenting.

--- threeLargestNumbers.py
+++ threeLargestNumbers.py
@@ -1,17 +1,17 @@
 def findThreeLargestNumbers(array):
-    largest=None
-	secondLargest=None
-	thirdLargest=None
-	
-	for x in array:
-		if largest==None or x>largest:
-			thirdLargest = secondLargest
-			secondLargest = largest
-			largest = x
-		elif secondLargest==None or x>secondLargest:
-			thirdLargest = secondLargest
-			secondLargest = x
-		elif thirdLargest==None or x>thirdLargest:
-			thirdLargest = x
-	
-	return [thirdLargest, secondLargest, largest]
+    largest = None
+    secondLargest = None
+    thirdLargest = None
+
+    for x in array:
+        if largest == None or x > largest:
+            thirdLargest = secondLargest
+            secondLargest = largest
+            largest = x
+        elif secondLargest==None or x>secondLargest:
+            thirdLargest = secondLargest
+            secondLargest = x
+        elif thirdLargest==None or x>thirdLargest:
+            thirdLargest = x
+
+    return [thirdLargest, secondLargest, largest]
