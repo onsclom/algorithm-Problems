b688a85c60232eecaf786d83caae8ba239505871
Austin Merrick <austinmerrick98@gmail.com>
2019-10-07 20:16:28 -0700

    Better space complexity for fib.

--- fibonacci.py
+++ fibonacci.py
@@ -8,10 +8,23 @@
 # 		return getNthFib(n-1)+getNthFib(n-2)
 	
 #dynamic programming solution O(n), but bad space
-memo = {1:0, 2:1}
-def getNthFib(n):
-	if n not in memo:
-		memo[n] = getNthFib(n-1)+getNthFib(n-2)
-	return memo[n]
+# memo = {1:0, 2:1}
+# def getNthFib(n):
+# 	if n not in memo:
+# 		memo[n] = getNthFib(n-1)+getNthFib(n-2)
+# 	return memo[n]
 
 #can still eliminate some memory by only keeping last two numbers!!!
+def getNthFib(n):
+	cur=1
+	val=0
+	first, second = 0, 1
+	while cur <= n:
+		if cur == 2:
+			val=1
+		elif cur > 2:
+			val=first+second
+			first = second
+			second = val
+		cur+=1
+	return val
