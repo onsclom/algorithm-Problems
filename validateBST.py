23f0bb5647cf875ca0d18d1cb9ee017aa3f01100
Austin Merrick <austinmerrick98@gmail.com>
2019-10-12 17:25:47 -0700

    Cleaning up validate BST.

--- validateBST.py
+++ validateBST.py
@@ -6,5 +6,5 @@ def validateBst(tree, min = -inf, max = inf):
 	
 	if tree.value >= min and tree.value < max:
 		return validateBst(tree.left, min, tree.value) and validateBst(tree.right, tree.value, max)
-	else:
-		return False
+	
+	return False
