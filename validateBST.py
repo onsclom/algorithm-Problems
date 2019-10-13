458dffd5bc6ab8c1bcaa31e45530ab9e42b2423c
Austin Merrick <austinmerrick98@gmail.com>
2019-10-12 17:24:08 -0700

    Made solution for validate BST.

---
+++ validateBST.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def validateBst(tree, min = -inf, max = inf):
+	if tree == None:
+		return True
+	
+	if tree.value >= min and tree.value < max:
+		return validateBst(tree.left, min, tree.value) and validateBst(tree.right, tree.value, max)
+	else:
+		return False
